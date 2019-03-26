6bcc0aaaaedb0fe91e735abf2b761e28befa6e37
Aleksandr Busygin <persik665@yandex.ru>
2019-03-27 00:32:56 +0300

    не работает

--- src/main/java/util/AllureReporter.java
+++ src/main/java/util/AllureReporter.java
@@ -5,20 +5,18 @@ import gherkin.formatter.model.Result;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import steps.BaseSteps;
+
 import static util.Init.getDriver;
 
 public class AllureReporter extends AllureCucumberJvm{
 
     @Override
     public void result(Result result) {
-        if ("failed".equals(result.getStatus())) takeScreenshot(result);
+        if ("failed".equals(result.getStatus())) BaseSteps.takeScreenshot();
         super.result(result);
     }
 
-    public void takeScreenshot(Result step) {
-        if (getDriver() != null) {
-            Allure.LIFECYCLE.fire(new MakeAttachmentEvent(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES),
-                    "Скриншот в момент ошибки", "image/png"));
-        }
-    }
+
+
 }
